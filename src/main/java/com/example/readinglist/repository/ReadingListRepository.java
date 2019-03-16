390def3c5cde9aae776a721c7715a51dd1942091
Gary Su <gengwensu@yahoo.com>
2019-03-16 18:24:01 -0400

    Adding Book and JPA

---
+++ src/main/java/com/example/readinglist/repository/ReadingListRepository.java
@@ -0,0 +1,13 @@
+package com.example.readinglist.repository;
+
+import com.example.readinglist.datastruct.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ *
+ */
+public interface ReadingListRepository extends JpaRepository<Book, Long> {
+    List<Book> findByReader(String reader);
+}

17b44af2e1cc7f1d8a404c2f2e05fb7c9fc3c23a
Gary Su <gengwensu@yahoo.com>
2019-03-16 20:43:25 -0400

    Correct typos etc

--- src/main/java/com/example/readinglist/controller/ReadingListController.java
+++ src/main/java/com/example/readinglist/controller/ReadingListController.java
@@ -26,13 +26,13 @@ public class ReadingListController {
 
     @RequestMapping(value = "/{reader}", method = RequestMethod.GET)
     public String readersBooks(@PathVariable("reader") String reader, Model model) {
-        List<Book> readingList = readingListRepository.findByReader((reader);
+        List<Book> readingList = readingListRepository.findByReader(reader);
         if (readingList != null) model.addAttribute("books", readingList);
         return "readingList";
     }
 
     @RequestMapping(value = "/{reader}", method = RequestMethod.POST)
-    public String addToReadingList(@PathVariable("reader") String reader,, Book book) {
+    public String addToReadingList(@PathVariable("reader") String reader, Book book) {
         book.setReader(reader);
         readingListRepository.save(book);
         return "redirect:/{reader}";
